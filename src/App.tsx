16b6ec3a225c472b1933523665be236f863ec70c
Refren86 <denis1975shvaiber@gmail.com>
2022-08-08 23:29:43 +0300

    Added lazy loading for slight optimization

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch } from 'react-redux';
+import { Routes, Route } from 'react-router-dom';
+import { useEffect, lazy, Suspense } from 'react';
 
-import { Routes, Route } from "react-router-dom";
+import Spinner from './components/spinner/spinner';
+import { checkUserSession } from './store/user/user.action';
 
-import Home from "./routes/home/home";
-import Shop from "./routes/shop/shop";
-import Checkout from "./routes/checkout/checkout";
-import Navigation from "./routes/navigation/navigation";
-import Authentication from "./routes/authentication/authentication";
-
-import { checkUserSession } from "./store/user/user.action";
+const Home = lazy(() => import('./routes/home/home'));
+const Authentication = lazy(
+  () => import('./routes/authentication/authentication')
+);
+const Shop = lazy(() => import('./routes/shop/shop'));
+const Checkout = lazy(() => import('./routes/checkout/checkout'));
+const Navigation = lazy(() => import('./routes/navigation/navigation'));
 
 const App = () => {
   const dispatch = useDispatch();
@@ -19,14 +21,16 @@ const App = () => {
   }, [dispatch]);
 
   return (
-    <Routes>
-      <Route path="/" element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path="shop/*" element={<Shop />} />
-        <Route path="auth" element={<Authentication />} />
-        <Route path="checkout" element={<Checkout />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<Spinner />}>
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path="shop/*" element={<Shop />} />
+          <Route path="auth" element={<Authentication />} />
+          <Route path="checkout" element={<Checkout />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
 
